02df756d4100c9cb6aef6c50ca15a972d6d4b2e8
AlegriaForman <alegriaforman@gmail.com>
2016-06-05 15:25:24 -0700

    revise entry.js

--- client/app/js/entry.js
+++ client/app/js/entry.js
@@ -3,11 +3,22 @@ const demoApp = angular.module('demoApp', [require('angular-route')]);
 
 require('./services')(demoApp);
 require('./trails')(demoApp);
+require('./userprofile')(demoApp);
 require('./auth')(demoApp);
 
 demoApp.config(['$routeProvider', function($rp) {
   $rp
     .when('/trails', {
+      templateUrl: 'templates/trails/views/trails_view.html',
+      controller: 'TrailsController',
+      controllerAs: 'trailsctrl'
+    })
+    .when('/userprofile', {
+      templateUrl: 'templates/userprofile/views/userprofile_view.html',
+      controller: 'UserprofileController',
+      controllerAs: 'userprofilectrl'
+    })
+    .when('/favorites', {
       templateUrl: 'templates/trails/views/trails_view.html',
       controller: 'TrailsController',
       controllerAs: 'trailsctrl'
