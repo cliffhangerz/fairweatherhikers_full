0e9e8bcf1070c81ed0785fc4a292b17e0a748445
AlegriaForman <alegriaforman@gmail.com>
2016-06-05 15:24:56 -0700

    delete feh_comm_service.js

--- client/app/js/auth/services/fwh_comm_service.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(app) {
-  app.factory('commService', ['$rootScope', function() {
-    return {
-      var remote = new Resource(this.trails, this.errors, baseUrl + '/api/trails', {errMsgs: {getAll: 'Trails Error.'}});
-      this.getAll = remote.getAll.bind(remote);
-    };
-  }]);
-};
