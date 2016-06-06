dd6ba81075006d5a2cfd84f52c09da59b3a8f1f6
KMSkelton <kmskelton@outlook.com>
2016-06-06 02:13:56 -0700

    demonstrates the loop, doesn't save

--- api_server/lib/forecast_updater.js
+++ api_server/lib/forecast_updater.js
@@ -5,10 +5,10 @@ var lon = 122.3493;
 var API_KEY = 'fdbc915b0ead7e316556b1de658613ef';
 var fs = require('fs');
 
-module.exports = exports = function(lat,lon) {
+module.exports = exports = function(lat, lon) {
 
   var url = 'https://api.forecast.io/forecast/' + API_KEY + '/' + lat + ',-' + lon
-  console.log(url);
+
   var request = https.get(url, (response) => {
     response.setEncoding('utf-8');
     var buffer = '';
@@ -23,38 +23,38 @@ module.exports = exports = function(lat,lon) {
       forecast = JSON.parse(buffer);
 
       console.log("forecast is:", forecast);
-      console.log("does this get the summary? ", forecast.hourly.data[0].summary);
 
       // Let's add some data to the db
       // It should overwrite whatever is there because it will change each day
-for (var i = 0; i < forecast.daily.data.length; i ++) {
-      var schemeifiedData = {
-        lat: forecast.latitude,
-        lon: forecast.longitude,
-        day: Date(forecast.daily.data[i].time),
-        summary: forecast.daily.data[i].summary,
-        icon: forecast.daily.data[i].icon,
-        precipProbability: forecast.daily.data[i].precipProbability,
-        temperatureMin: forecast.daily.data[i].temperatureMin,
-        temperatureMax: forecast.daily.data[i].temperatureMax
-      };
-}
-
-      var postOptions = {
-        hostname: 'localhost',
-        port: '3000',
-        path: '/api/forecast',
-        method: 'POST',
-        json: true,
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      };
-      var postData = http.request(postOptions, (res) => {});
-      var preppedData = JSON.stringify(schemeifiedData);
-      console.log('preppedData: ', preppedData);
-      postData.write(preppedData);
-      postData.end();
+      for (var i = 0; i < forecast.daily.data.length; i++) {
+        var schemeifiedData = {
+          lat: forecast.latitude,
+          lon: forecast.longitude,
+          day: forecast.daily.data[i].time,
+          summary: forecast.daily.data[i].summary,
+          icon: forecast.daily.data[i].icon,
+          precipProbability: forecast.daily.data[i].precipProbability,
+          temperatureMin: forecast.daily.data[i].temperatureMin,
+          temperatureMax: forecast.daily.data[i].temperatureMax
+        };
+        console.log('schemeifiedData is', schemeifiedData);
+        var postOptions = {
+          hostname: 'localhost',
+          port: '3000',
+          path: '/api/forecast',
+          method: 'POST',
+          json: true,
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        };
+        var postData = http.request(postOptions, (res) => {
+          var preppedData = JSON.stringify(schemeifiedData);
+          console.log('preppedData: ', preppedData);
+          postData.write(preppedData);
+          postData.end();
+        });
+      }
     });
   });
-}
+};
