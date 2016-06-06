22261ff29ff259d8679f9122452eca049744c65b
Gene Troy <genetroy@hotmail.com>
2016-06-06 11:03:02 -0700

    rebuild gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,10 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
-const maps = require('gulp-sourcemaps');
-const protractor = require('gulp-protractor').protractor;
-const cp = require('child_process');
 const webpack = require('webpack-stream');
-const eslint = require('gulp-eslint');
-const mocha = require('gulp-mocha');
-const mongoose = require('mongoose');
-const mongoUri = 'mongodb://localhost/bands_test';
-
-var lintFiles = ['**/*.js', '!node_modules/**', '!build/**', '!**/*spec.js',
-'!**/*test.js', '!test/**bundle.**', '!*.conf.js'];
 var statFiles = ['./client/app/**/*.html'];
-var children = [];
 
 gulp.task('webpack:dev', () => {
   gulp.src('client/app/js/entry.js')
     .pipe(webpack( {
-      devtool: 'source-map',
       output: {
         filename: 'bundle.js'
       }
@@ -30,11 +17,4 @@ gulp.task('static:dev', () => {
     .pipe(gulp.dest('client/build'));
 });
 
-gulp.task('lint:dev', () => {
-  return gulp.src(lintFiles)
-    .pipe(eslint())
-    .pipe(eslint.format());
-});
-
 gulp.task('build', ['webpack:dev', 'static:dev']);
-gulp.task('lint', ['lint:dev']);
