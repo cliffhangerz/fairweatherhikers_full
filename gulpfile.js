dbad9f6a9ebe0bd9534fb7bc84dac022cce3c887
Gene Troy <genetroy@hotmail.com>
2016-06-07 13:49:30 -0700

    fix typos in entry.js, clean up stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,16 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
+const maps = require('gulp-sourcemaps');
+const protractor = require('gulp-protractor').protractor;
+const cp = require('child_process');
 const webpack = require('webpack-stream');
 const eslint = require('gulp-eslint');
 const mocha = require('gulp-mocha');
 const mongoose = require('mongoose');
 const mongoUri = 'mongodb://localhost/bands_test';
 
-var lintFiles = [
-  '**/*.js',
-  '!node_modules/**',
-  '!**/build/**',
-  '!**/*spec.js',
-  '!**/*test.js',
-  '!test/**bundle.**',
-  '!*.conf.js'
-];
+var lintFiles = ['**/*.js', '!node_modules/**', '!**/build/**', '!**/*spec.js',
+'!**/*test.js', '!test/**bundle.**', '!*.conf.js'];
 var statFiles = ['./client/app/**/*.html', './client/app/images/**.*'];
 var children = [];
 
@@ -26,6 +23,7 @@ function killcp() {
 gulp.task('webpack:dev', () => {
   gulp.src('client/app/js/entry.js')
     .pipe(webpack( {
+      devtool: 'source-map',
       output: {
         filename: 'bundle.js'
       }
@@ -115,6 +113,9 @@ gulp.task('lint:dev', () => {
 //     });
 // });
 
+// gulp.task('sass:watch', () => {
+//   gulp.watch('./*scss', ['sass:dev']);
+// });
 // gulp.task('test', ['protractor:test', 'webpack:protractor']);
 // gulp.task('default', ['build', 'lint', 'test']);
 gulp.task('build', ['webpack:dev', 'static:dev', 'sass:dev']);
