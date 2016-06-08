aecdaa2387a6966ba5f2d93be9516c7475214109
Gene Troy <genetroy@hotmail.com>
2016-06-07 17:02:29 -0700

    adding markers

--- client/app/js/map/controllers/map_controller.js
+++ client/app/js/map/controllers/map_controller.js
@@ -11,7 +11,8 @@ module.exports = function(app) {
         latitude: centerLatitude,
         longitude: centerLongitude
       },
-      zoom: zoom
+      zoom: zoom,
+      bounds: {}
     };
 
     $scope.circles = [
@@ -41,7 +42,40 @@ module.exports = function(app) {
       }
     ];
 
+    var createRandomMarker = function(i, bounds, idKey) {
+      var lat_min = bounds.southwest.latitude,
+        lat_range = bounds.northeast.latitude - lat_min,
+        lng_min = bounds.southwest.longitude,
+        lng_range = bounds.northeast.longitude - lng_min;
 
+      if (idKey == null) {
+        idKey = "id";
+      }
+
+      var latitude = lat_min + (Math.random() * lat_range);
+      var longitude = lng_min + (Math.random() * lng_range);
+      var ret = {
+        latitude: latitude,
+        longitude: longitude,
+        title: 'm' + i
+      };
+      ret[idKey] = i;
+      return ret;
+    };
+    $scope.randomMarkers = [];
+    // Get the bounds from the map once it's loaded
+    $scope.$watch(function() {
+      return $scope.map.bounds;
+    }, function(nv, ov) {
+      // Only need to regenerate once
+      if (!ov.southwest && nv.southwest) {
+        var markers = [];
+        for (var i = 0; i < 50; i++) {
+          markers.push(createRandomMarker(i, $scope.map.bounds))
+        }
+        $scope.randomMarkers = markers;
+      }
+    }, true);
 
 
 
