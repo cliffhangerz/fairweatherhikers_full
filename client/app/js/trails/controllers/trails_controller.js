bead3eb57b2ecfe1d6f433033d3d4027b2edb3b7
KMSkelton <kmskelton@outlook.com>
2016-06-09 02:07:24 -0700

    changed variable name

--- client/app/js/trails/controllers/trails_controller.js
+++ client/app/js/trails/controllers/trails_controller.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
   app.controller('TrailsController', ['fwhResource', function(Resource) {
     this.trails = [];
     this.errors = [];
-    this.stupid = '';
+    this.selectedForecast = '';
     var remote = new Resource(this.trails, this.errors, baseUrl + '/api/trails', {errMsgs: {getAll: 'Trails Error.'}});
     this.getAll = remote.getAll.bind(remote);
 
@@ -23,14 +23,14 @@ module.exports = function(app) {
     };
     this.removeTrail = remote.remove.bind(remote);
     this.getAll();
-    this.getClickedName = function(elem) {
+    this.updateSelectedForecast = function(elem) {
         var clickedName;
         var clickedTemp;
         clickedName = elem.trailName;
         clickedTemp = elem.weather.data[0].temperatureMax;
-        this.stupid = elem;
-        alert(this.stupid.trailName);
-        document.getElementById('todayTemp').innerHTML = this.stupid.trailName +' Today\'s high: ' + this.stupid.weather.data[0].temperatureMax;
+        this.selectedForecast = elem;
+        alert(this.selectedForecast.trailName);
+        document.getElementById('todayTemp').innerHTML = this.selectedForecast.trailName +' Today\'s high: ' + this.selectedForecast.weather.data[0].temperatureMax;
         alert(clickedName);
         alert(clickedTemp);
       };
