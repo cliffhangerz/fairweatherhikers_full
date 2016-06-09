b11aea7e56f7511fefd9e068bfa227efbab6cc90
KMSkelton <kmskelton@outlook.com>
2016-06-09 02:01:29 -0700

    can click on trail name and the trail's name and today's high temp will display at the top of the screen

--- client/app/js/trails/controllers/trails_controller.js
+++ client/app/js/trails/controllers/trails_controller.js
@@ -5,6 +5,7 @@ module.exports = function(app) {
   app.controller('TrailsController', ['fwhResource', function(Resource) {
     this.trails = [];
     this.errors = [];
+    this.stupid = '';
     var remote = new Resource(this.trails, this.errors, baseUrl + '/api/trails', {errMsgs: {getAll: 'Trails Error.'}});
     this.getAll = remote.getAll.bind(remote);
 
@@ -22,5 +23,16 @@ module.exports = function(app) {
     };
     this.removeTrail = remote.remove.bind(remote);
     this.getAll();
+    this.getClickedName = function(elem) {
+        var clickedName;
+        var clickedTemp;
+        clickedName = elem.trailName;
+        clickedTemp = elem.weather.data[0].temperatureMax;
+        this.stupid = elem;
+        alert(this.stupid.trailName);
+        document.getElementById('todayTemp').innerHTML = this.stupid.trailName +' Today\'s high: ' + this.stupid.weather.data[0].temperatureMax;
+        alert(clickedName);
+        alert(clickedTemp);
+      };
   }]);
 };
