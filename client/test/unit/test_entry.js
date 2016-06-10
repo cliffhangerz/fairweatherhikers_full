d35d1131101db352cac90fa5dd20e368cff3002b
AlegriaForman <alegriaforman@gmail.com>
2016-06-09 17:29:18 -0700

    delete aboutController

--- client/test/unit/test_entry.js
+++ client/test/unit/test_entry.js
@@ -2,5 +2,5 @@ angular = require('angular');
 require('angular-mocks');
 require('../../app/js/entry');
 require('./handle_error_test');
-require('./trails_controller_test');
+// require('./trails_controller_test');
 require('./fwh_resource_test');
